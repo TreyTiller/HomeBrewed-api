a0cdef7b6459a43f3cfe3365657c397f66436515
Trey <treytiller@outlook.com>
2019-11-29 21:39:59 -0500

    no message

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://TriggaTrey@localhost/HomeBrewed-DB',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://TriggaTrey@localhost/HomeBrewed-DB',
     JWT_SECRET: process.env.JWT_SECRET || 'this-is-boring',
   }
